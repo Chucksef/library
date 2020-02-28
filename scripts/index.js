29989f8b1b0ee527783bcc3c2f27cbc94d0ae337
chucksef <chucksef@gmail.com>
2020-02-28 16:12:11 -0700

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+class Book {
+    constructor(title, author, pubDate, owned="true", read="false") {
+        self.title = title;
+        self.author = author;
+        self.pubDate = pubDate;
+        self.owned = owned;
+        self.read = read;
+    }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
