277339b69cfd21b034ab9ef6e7765c92efa7d0ec
chucksef <chucksef@gmail.com>
2020-03-03 10:02:29 -0700

    basic javascript working

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,29 @@
-let myLibrary = [];
+window.addEventListener('load', main(), false);
 
 class Book {
-    constructor(title, author, pubDate, status="owned") {
-        self.title = title;
-        self.author = author;
-        self.pubDate = pubDate;
-        self.status = status;
-    }
+  constructor(title, author, pubDate, status="unread", color=generateColor()) {
+    self.title = title;
+    self.author = author;
+    self.pubDate = pubDate;
+    self.status = status;
+    self.color = color;
+  }
 }
 
 function addBookToLibrary() {
   // do stuff here
+}
+
+function generateColor() {
+  
+}
+
+function main() {
+  //basic variables for main function
+  var library = document.querySelector("#library");
+  var addBook = document.querySelector("#add_book");
+  let myLibrary = [];
+  
+  addBook.addEventListener("click", addBookToLibrary());
+  
 }
